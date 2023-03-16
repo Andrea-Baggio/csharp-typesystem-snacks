2e76640d801a808652e52194c638f84287dc7890
Andrea-Baggio <andreabaggio800@gmail.com>
2023-03-16 16:16:14 +0100

    Snack 6

--- Program.cs
+++ Program.cs
@@ -1,2 +1,116 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Word!");
+﻿//Console.Write("Inserisci il primo numero: ");
+//int num1 = int.Parse(Console.ReadLine());
+
+//Console.Write("Inserisci il secondo numero: ");
+//int num2 = int.Parse(Console.ReadLine());
+
+//int maggiore = num1;
+
+//if (num2 > num1)
+//{
+//    maggiore = num2;
+//}
+
+//Console.WriteLine("Il maggiore numero inserito è: " + maggiore);
+
+//----------------------------------------------------------------
+
+//Console.WriteLine("Inserisci la prima parola: ");
+//string word1 = (Console.ReadLine());
+
+//Console.WriteLine("Inserisci la seconda parola: ");
+//string word2 = (Console.ReadLine());
+
+//if (word1.Length > word2.Length)
+//{
+//    Console.WriteLine("La parola più lunga è: " + word1);
+//}
+//else if (word1.Length < word2.Length)
+//{
+//    Console.WriteLine("La parola più lunga è: " + word2);
+//}
+//else
+//{
+//    Console.WriteLine("Le parole hanno la stessa lunghezza");
+//}
+
+//----------------------------------------------------------------
+
+//int sum = 0;
+
+//for (var i = 0; i < 10; i++)
+//{
+//    Console.Write("Inserisci un numero: ");
+//    int num = Convert.ToInt32(Console.ReadLine());
+//    sum += num;
+//}
+
+//Console.WriteLine("la somma di tutti i numeri inseriti è: " + sum);
+
+//----------------------------------------------------------------
+
+//int sum = 0;
+
+//for (int i = 2; i <= 10; i++)
+//{
+//    sum += i;
+//}
+
+//Console.WriteLine("La somma dei numeri da 2 a 10 è: " + sum);
+
+//double average = (double)sum / 9;
+//Console.WriteLine("La media della somma dei numeri da 2 a 10 è: " + average);
+
+//----------------------------------------------------------------
+
+//Console.Write("Inserisci un numero positivo: ");
+//int num = int.Parse(Console.ReadLine());
+
+//if (num > 0)
+//{
+//    if (num % 2 == 0)
+//    {
+//        Console.WriteLine("Complimenti per la scelta: " + num);
+//    }
+//    else
+//    {
+//        Console.WriteLine("Questo numero è dispari, io odio i numeri dispari: " + (num + 1));
+//    }
+//}
+//else
+//{
+//    Console.WriteLine("Il numero inserito non è valido. Inserire un numero positivo diverso da zero.");
+//}
+
+//----------------------------------------------------------------
+
+string[] invitati = { "Nick Carraway", "Jay Gatsby", "Daisy Buchanan", "Tom Buchanan", "Jordan Baker", "Myrtle Wilson" };
+
+Console.Write("Inserisci il tuo nome: ");
+string name = Console.ReadLine();
+
+bool guest = false;
+
+for (int i = 0; i < invitati.Length; i++)
+{
+    if (invitati.Contains(name))
+    {
+        guest = true;
+        break;
+    }
+}
+
+if (guest)
+{
+    Console.WriteLine("Sei stato invitato alla festa del Grande Gatsby!");
+}
+else
+{
+    Console.WriteLine("Mi dispiace, il tuo nome non è presente nell'elenco degli invitati.");
+}
+
+//----------------------------------------------------------------
+
+
+
+
